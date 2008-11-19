fd3940ac4f01176d7a8c0e9edaafa41a9ee71735
Clemens Muchitsch <muchitsch@ict.tuwien.ac.at>
2008-11-19 23:48:23 +0000

    1st in
    git-svn-id: svn+ssh://vesta.ict.tuwien.ac.at/home/prj/ARS/SVNRoot/SVNRepository/ARS/PA/BFG30@1400 b2bfa92a-82f1-a04d-93ea-50cd9150fe96

---
+++ BFG/src/bfg/sim/clsMain.java
@@ -0,0 +1,27 @@
+package bfg.sim;
+
+import sim.engine.*;
+
+
+/**
+ * @author muchitsch
+ * Main function for simulation
+ */
+public class clsMain extends SimState{
+
+	public clsMain(long seed){
+		super(seed);
+	}
+
+	public void start()
+	{
+		super.start();
+	}
+	
+	public static void main(String[] args)
+	{
+		doLoop(clsMain.class, args);
+		System.exit(0);
+	}
+	
+}// end class
---
+++ BFG/src/bfg/sim/clsMainWithUI.java
@@ -0,0 +1,100 @@
+package bfg.sim;
+
+import sim.display.*;
+import sim.engine.*;
+import javax.swing.*;
+import sim.portrayal.*;
+import sim.portrayal.simple.*;
+import sim.portrayal.continuous.*;
+import java.awt.Color;
+
+
+/**
+ * @author muchitsch
+ * Main function for Simulation, UI part
+ */
+public class clsMainWithUI extends GUIState{
+
+	/**
+	 * GUI widget which holds some number of field portrayals, 
+	 * usually layered on top of one another
+	 */
+	public Display2D moDisplay;
+	/**
+	 * window to hold Display2D
+	 */
+	public JFrame moDisplayFrame;
+	/**
+	 * 
+	 */
+	ContinuousPortrayal2D moBFGArena = new ContinuousPortrayal2D();
+	
+	public static void main(String[] args){
+		Console oConsole = new Console(new clsMainWithUI());
+		oConsole.setVisible(true);
+	}
+	public clsMainWithUI() { super(new clsMain( System.currentTimeMillis())); }
+	public clsMainWithUI(SimState oState) { super(oState); }
+	
+	/**
+	 * returns the title bar of the console
+	 * @return String
+	 */
+	public static String getName() { return "... BFG V3.0 ..."; } 
+
+	public void init(Controller oController){
+		
+		super.init(oController);
+		
+		moDisplay = new Display2D(600,600,this,1);
+		moDisplay.setClipping(false);
+		
+		moDisplayFrame = moDisplay.createFrame();
+		moDisplayFrame.setTitle("BFG V3.0 Display");
+		
+		oController.registerFrame(moDisplayFrame);
+		moDisplayFrame.setVisible(true);
+		moDisplay.attach(moBFGArena, "Arena");
+	}
+	
+	public void quit(){
+		super.quit();
+		if (moDisplayFrame!=null) 
+			moDisplayFrame.dispose();
+		moDisplayFrame = null;
+		moDisplay = null;
+	}
+	
+	public void start(){
+		super.start();
+		setupPortrayals();
+	}
+	
+	public void load(SimState oState){
+		super.load(oState);
+		setupPortrayals();
+	}
+	
+	/** Here we tell the Portrayal which field it is portraying and how it’s doing it. 
+	 * We’ll also reset the display so it reregisters itself with the console in 
+	 * preparation for being stepped each timestep 
+	 */
+	public void setupPortrayals(){
+		moDisplay.reset();
+		moDisplay.setBackdrop(Color.white);
+		// redraw the display
+		moDisplay.repaint();
+	}
+	
+	public Object getSimulationInspectedObject(){ 
+		return state; 
+	}
+	
+	public Inspector getInspector(){
+		//Override to get constantly updating inspectors = volatile
+	    Inspector oInspector = super.getInspector();
+	    oInspector.setVolatile(true);
+	    return oInspector;
+    }
+	
+}//end class
