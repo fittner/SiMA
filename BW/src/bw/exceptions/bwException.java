afb8226a3744d43184dd459d924c2aff55b60ddb
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-02-16 13:43:44 +0000

    exception package added

---
+++ BW/src/bw/exceptions/bwException.java
@@ -0,0 +1,18 @@
+/**
+ * @author deutsch
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.exceptions;
+
+/**
+ * TODO (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 
+ */
+public class bwException {
+
+}
