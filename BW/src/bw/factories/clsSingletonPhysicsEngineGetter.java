a434568fba1ee4f2b1dc0bffa788aed6aeaa4ae0
Heimo Zeilinger <zeilinger@ict.tuwien.ac.at>
2009-02-19 09:42:28 +0000

    make a physics engine getter singleton classs

---
+++ BW/src/bw/factories/clsSingletonPhysicsEngineGetter.java
@@ -0,0 +1,18 @@
+/**
+ * @author zeilinger
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.factories;
+
+/**
+ * TODO (zeilinger) - insert description 
+ * 
+ * @author zeilinger
+ * 
+ */
+public class clsSingletonPhysicsEngineGetter {
+
+}
