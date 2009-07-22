44abc304c51dc2b88c8601a3b1e3d4b9f521dd1f
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-22 13:24:30 +0000

    slow messenger type enum added

---
+++ BW/src/bw/utils/enums/eSlowMessenger.java
@@ -0,0 +1,21 @@
+/**
+ * @author deutsch
+ * 22.07.2009, 15:24:58
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.utils.enums;
+
+/**
+ * TODO (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 22.07.2009, 15:24:58
+ * 
+ */
+public enum eSlowMessenger {
+	TESTOSTERON,
+	BLOODSUGAR,
+}
