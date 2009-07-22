533dd65d6c42702cacd19f7a495867562055a0c3
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-22 21:01:54 +0000

    created

---
+++ BW/src/bw/utils/enums/eSensorsInternal.java
@@ -0,0 +1,23 @@
+/**
+ * @author deutsch
+ * 22.07.2009, 23:02:43
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.utils.enums;
+
+/**
+ * TODO (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 22.07.2009, 23:02:43
+ * 
+ */
+public enum eSensorsInternal {
+	HEALTH,
+	ENERGY,
+	STAMINA,
+	STOMACH,
+}
