11d7fca3edae38dcda2fcca3631aaf371d6635d9
Roland Lang <langr@ict.tuwien.ac.at>
2009-07-23 14:38:14 +0000

    enumDecisionType -> prjInterface

---
+++ DecisionUnitInterface/src/du/utils/enums/eDecisionType.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package du.utils.enums;
+
+/**
+ * @author langr
+ *
+ */
+public enum eDecisionType {
+
+	DU_DUMB_MIND_A,
+	DU_DUMB_MIND_ORIENTATION,
+
+	DU_HARE_MIND_IFTHENELSE,
+	DU_HARE_MIND_JADEX,
+	DU_HARE_MIND_JAM,
+
+	DU_LYNX_MIND_JADEX,
+	DU_LYNX_MIND_JAM,
+	DU_LYNX_MIND_IFTHENELSE,
+	
+	DU_REMOTE,
+	DU_RULE_BASED,
+	NONE
+	
+}
