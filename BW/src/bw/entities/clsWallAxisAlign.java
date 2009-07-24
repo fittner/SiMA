bcfa9ca7cf6d91fab54368e185e71adefe25b723
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-24 21:01:41 +0000

    axis align walls added.

---
+++ BW/src/bw/entities/clsWallAxisAlign.java
@@ -0,0 +1,61 @@
+/**
+ * @author langr
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.entities;
+
+import java.awt.Color;
+
+import bw.entities.tools.clsShapeCreator;
+import bw.utils.config.clsBWProperties;
+import bw.utils.enums.eShapeType;
+import enums.eEntityType;
+
+
+/**
+ * Mason representative (physics+renderOnScreen) for a wall.  
+ * 
+ * FIXME: CLEMENS - REIMPLEMENT PLEASE!!!!
+ * 
+ * @author langr
+ * 
+ */
+public class clsWallAxisAlign extends clsStationary  {
+    public clsWallAxisAlign(String poPrefix, clsBWProperties poProp) {
+    	super(poPrefix, poProp);
+    	
+    	applyProperties(poPrefix, poProp);
+    } 
+
+	public static clsBWProperties getDefaultProperties(String poPrefix) {
+		String pre = clsBWProperties.addDot(poPrefix);
+		clsBWProperties oProp = new clsBWProperties();
+		oProp.putAll(clsStationary.getDefaultProperties(pre) );
+		
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_TYPE, eShapeType.RECTANGLE.name());
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_WIDTH, 10);
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_HEIGHT, 10);
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_COLOR, Color.black);
+			
+		return oProp;
+	}	
+		
+	private void applyProperties(String poPrefix, clsBWProperties poProp){		
+		// nothing to do
+	}	
+	
+	/* (non-Javadoc)
+	 *
+	 * @author deutsch
+	 * 23.02.2009, 15:35:16
+	 * 
+	 * @see bw.clsEntity#setEntityType()
+	 */
+	@Override
+	protected void setEntityType() {
+		meEntityType = eEntityType.WALL;
+	}
+}
---
+++ BW/src/bw/entities/clsWallHorizontal.java
@@ -0,0 +1,56 @@
+/**
+ * @author tobias
+ * Jul 24, 2009, 10:50:57 PM
+ * 
+ * $Rev::                      $: Revision of last commit
+ * $Author::                   $: Author of last commit
+ * $Date::                     $: Date of last commit
+ */
+package bw.entities;
+
+import java.awt.Color;
+
+import bw.entities.tools.clsShapeCreator;
+import bw.utils.config.clsBWProperties;
+import bw.utils.enums.eShapeType;
+
+/**
+ * TODO (tobias) - insert description 
+ * 
+ * @author tobias
+ * Jul 24, 2009, 10:50:57 PM
+ * 
+ */
+public class clsWallHorizontal extends clsWallAxisAlign {
+	/**
+	 * TODO (tobias) - insert description 
+	 * 
+	 * @author tobias
+	 * Jul 24, 2009, 10:51:04 PM
+	 *
+	 * @param poPrefix
+	 * @param poProp
+	 */
+	public clsWallHorizontal(String poPrefix, clsBWProperties poProp) {
+		super(poPrefix, poProp);
+    	applyProperties(poPrefix, poProp);
+	}
+	
+	public static clsBWProperties getDefaultProperties(String poPrefix) {
+		String pre = clsBWProperties.addDot(poPrefix);
+		clsBWProperties oProp = new clsBWProperties();
+		oProp.putAll(clsStationary.getDefaultProperties(pre) );
+		
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_TYPE, eShapeType.RECTANGLE.name());
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_WIDTH, 10);
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_HEIGHT, 1);
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_COLOR, Color.black);
+		oProp.setProperty(pre+P_SHAPE+"."+clsShapeCreator.P_IMAGE_PATH, "/src/resources/images/wall1.jpg");
+		
+		return oProp;
+	}		
+	
+	private void applyProperties(String poPrefix, clsBWProperties poProp){		
+		// nothing to do
+	}
+}
