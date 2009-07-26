f7b8de664213513899009627735b05f9625ecd1a
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-26 15:35:14 +0000

    .* imports removed

--- BW/src/sim/clsBWMainEvSelector.java
+++ BW/src/sim/clsBWMainEvSelector.java
@@ -14,9 +14,12 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.TextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.*;
+
 
 
 /**
