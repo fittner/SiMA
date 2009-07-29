3fdf0385358a8fcb679e0b6e584f1b18ca512682
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-29 12:15:30 +0000

    changed code and comment templates

---
+++ TestApps/src/antforage/test.java
@@ -0,0 +1,35 @@
+/**
+ * test.java: TestApps - antforage
+ * 
+ * @author tobias
+ * Jul 29, 2009, 2:08:25 PM
+ */
+package antforage;
+
+/**
+ * DOCUMENT (tobias) - insert description 
+ * 
+ * @author tobias
+ * Jul 29, 2009, 2:08:25 PM
+ * 
+ */
+public class test {
+
+	/**
+	 * DOCUMENT (tobias) - insert description
+	 *
+	 * @author tobias
+	 * Jul 29, 2009, 2:08:25 PM
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO (tobias) - Auto-generated method stub
+
+	}
+	
+	public void testif() {
+		
+	}
+
+}
