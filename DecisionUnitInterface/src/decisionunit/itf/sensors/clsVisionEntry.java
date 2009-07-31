2f9d7e5a4ec6bacae192213df4efcfb2ed8aba7d
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-07-31 08:24:02 +0000

    changed entity id from int to string

--- DecisionUnitInterface/src/decisionunit/itf/sensors/clsVisionEntry.java
+++ DecisionUnitInterface/src/decisionunit/itf/sensors/clsVisionEntry.java
@@ -11,7 +11,7 @@ public class clsVisionEntry {
 	public clsPolarcoordinate moPolarcoordinate = new clsPolarcoordinate();
 	public eEntityType mnEntityType = eEntityType.UNDEFINED;
 	public eShapeType mnShapeType = eShapeType.UNDEFINED;
-	public int moEntityId = -1;
+	public String moEntityId = "";
 	public boolean mnAlive = false;
 	public Color moColor = null;
 	
