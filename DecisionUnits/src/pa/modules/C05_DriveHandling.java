f36766085a680f2723db169e3c0c3767e90838a2
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-08-11 13:27:05 +0000

    module containers added

---
+++ DecisionUnits/src/pa/modules/C05_DriveHandling.java
@@ -0,0 +1,18 @@
+/**
+ * C05_DriveHandling.java: DecisionUnits - pa.modules
+ * 
+ * @author deutsch
+ * 11.08.2009, 15:28:05
+ */
+package pa.modules;
+
+/**
+ * DOCUMENT (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 11.08.2009, 15:28:05
+ * 
+ */
+public class C05_DriveHandling {
+
+}
---
+++ DecisionUnits/src/pa/modules/C06_AffectGeneration.java
@@ -0,0 +1,18 @@
+/**
+ * C06_AffectGeneration.java: DecisionUnits - pa.modules
+ * 
+ * @author deutsch
+ * 11.08.2009, 15:28:15
+ */
+package pa.modules;
+
+/**
+ * DOCUMENT (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 11.08.2009, 15:28:15
+ * 
+ */
+public class C06_AffectGeneration {
+
+}
