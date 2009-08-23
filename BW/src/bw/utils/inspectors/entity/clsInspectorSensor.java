5bf1da7b8332fc035294fa0ee08013d11e9ff431
Heimo Zeilinger <zeilinger@ict.tuwien.ac.at>
2009-08-23 14:08:41 +0000

    Adds a first version of a Sensor Inspector. Actually only the vision sensor is displayed

--- BW/src/bw/utils/inspectors/entity/clsInspectorSensor.java
+++ BW/src/bw/utils/inspectors/entity/clsInspectorSensor.java
@@ -8,9 +8,22 @@
  */
 package bw.utils.inspectors.entity;
 
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+
+import enums.eSensorExtType;
 import sim.display.GUIState;
+import sim.physics2D.physicalObject.PhysicalObject2D;
 import sim.portrayal.Inspector;
 import sim.portrayal.LocationWrapper;
+import sim.util.gui.PropertyField;
+import ARSsim.physics2D.physicalObject.clsCollidingObject;
+import bw.body.io.sensors.ext.clsSensorVision;
+import bw.body.itfget.itfGetSensorEngine;
 import bw.entities.clsEntity;
 
 
@@ -31,8 +44,13 @@ public class clsInspectorSensor extends Inspector {
 	 * 22.07.2009, 15:44:47
 	 */
 	private static final long serialVersionUID = 1L;
+	public Inspector moOriginalInspector;
+	LocationWrapper moWrapper;
+	GUIState moGuiState;
 	
+	private PropertyField moProp1;
 	clsEntity moHostEntity; 
+	clsSensorVision moEntityVision; 
 	/**
 	 * DOCUMENT (zeilinger) - insert description 
 	 * 
@@ -44,13 +62,44 @@ public class clsInspectorSensor extends Inspector {
 	 * @param guiState
 	 * @param poEntity
 	 */
-	public clsInspectorSensor(Inspector originalInspector,
-							  LocationWrapper wrapper, 
-							  GUIState guiState, 
+	public clsInspectorSensor(Inspector poOriginalInspector,
+							  LocationWrapper poWrapper, 
+							  GUIState poGuiState, 
 					          clsEntity poEntity) {
-     		// TODO (zeilinger) - Auto-generated constructor stub
+		
+		moOriginalInspector = poOriginalInspector;
+		moWrapper = poWrapper;
+		moGuiState = poGuiState;
+		moHostEntity = poEntity;
+		
+		moEntityVision = (clsSensorVision)((itfGetSensorEngine)poEntity)
+									.getSensorEngine().getMeRegisteredSensors().get(eSensorExtType.VISION); 
+		
+		//get the default things
+		clsInspectorDefault moDefaultInspector = new clsInspectorDefault(poOriginalInspector, poWrapper, poGuiState, moHostEntity);
+		add(moDefaultInspector,  BorderLayout.AFTER_LAST_LINE);
+		
+		//inspected fields....
+		Box oBox1 = new Box(BoxLayout.Y_AXIS);
+		
+		moProp1 = new  PropertyField("Objects in Sight","", false, null, PropertyField.SHOW_TEXTFIELD);
+		oBox1.add(moProp1, BorderLayout.AFTER_LAST_LINE);
+	
+
+		add(oBox1, BorderLayout.AFTER_LAST_LINE);
+			
 	}
 
+	public String getString(ArrayList<clsCollidingObject> ListTemp){
+		String dummyString = ""; 
+		
+		Iterator <clsCollidingObject> itrCO = ListTemp.iterator(); 
+		while(itrCO.hasNext()){
+				PhysicalObject2D oCollider= itrCO.next().moCollider; 
+				dummyString = dummyString + (oCollider.index+"\n");
+		}
+		return dummyString; 
+	}
 
 	/* (non-Javadoc)
 	 *
@@ -62,6 +111,7 @@ public class clsInspectorSensor extends Inspector {
 	@Override
 	public void updateInspector() {
 		// TODO (zeilinger) - Auto-generated method stub
-		
+		String tempString = getString(moEntityVision.getSensorData());// moSensorData.getMeDetectedObject().values()); 
+		moProp1.setValue(tempString);
 	}
 }
