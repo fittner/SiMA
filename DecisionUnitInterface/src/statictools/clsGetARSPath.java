f8b6e1dad1ff640dca0ef8434f143caecf175ff9
Stefan Kohlhauser <kohlhauser@ict.tuwien.ac.at>
2009-08-28 20:11:04 +0000

    Adapted Unix path to match dokuwiki

--- DecisionUnitInterface/src/statictools/clsGetARSPath.java
+++ DecisionUnitInterface/src/statictools/clsGetARSPath.java
@@ -4,7 +4,7 @@ public class clsGetARSPath {
 	public static String getArsPath()
 	{
 		if (System.getProperty("file.separator").equals("/"))
-			return System.getProperty("user.home") + "/ARS/PA/BWv1"; // Unix
+			return System.getProperty("user.home") + "/SVN/ARS/PA/BWv1"; // Unix
 		else if (System.getProperty("file.separator").equals("\\"))
 			return "S:/ARS/PA/BWv1"; // Windows
 		else throw new NullPointerException("Spooky OS detected, can't find ARS-Root-Dir.");
