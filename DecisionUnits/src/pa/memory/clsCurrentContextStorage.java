f855f322d324e55cc9aedaa32e89d63dead9bb4f
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2009-10-07 15:15:59 +0000

    current context storage finished

--- DecisionUnits/src/pa/memory/clsCurrentContextStorage.java
+++ DecisionUnits/src/pa/memory/clsCurrentContextStorage.java
@@ -6,7 +6,13 @@
  */
 package pa.memory;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import bfg.tools.clsMutableDouble;
+import pa.datatypes.clsThingPresentationSingle;
 import config.clsBWProperties;
+import enums.pa.eContext;
 
 /**
  * DOCUMENT (deutsch) - insert description 
@@ -16,8 +22,10 @@ import config.clsBWProperties;
  * 
  */
 public class clsCurrentContextStorage {
+	private TreeMap<clsThingPresentationSingle, clsMutableDouble> moStorage; 
+	
 	public clsCurrentContextStorage(String poPrefix, clsBWProperties poProp) {
-		
+		moStorage = new TreeMap<clsThingPresentationSingle, clsMutableDouble>();
 		applyProperties(poPrefix, poProp);
     }
     
@@ -37,5 +45,61 @@ public class clsCurrentContextStorage {
 		//oProp.setProperty(pre+P_SENSOR+"."+clsSensorVision.P_SENSOR_ANGLE, 1.99 * Math.PI );
 		
 		return oProp;
+    }
+    
+    public void setContextRatio(clsThingPresentationSingle poTPContext, double prRatio) {
+    	if (0>prRatio || prRatio>1) {
+    		throw new java.lang.IllegalArgumentException("0<="+prRatio+"<=1");
+    	}
+    	
+    	if (!poTPContext.meContentType.equals(eContext.class.getName())) {
+    		throw new java.lang.IllegalArgumentException("TP not of type "+eContext.class.getName());
+    	}
+    	
+    	clsMutableDouble oValue = moStorage.get(poTPContext);
+    	
+    	if (oValue != null) {
+    		oValue.set(prRatio);
+    	} else {
+    		oValue = new clsMutableDouble(prRatio);
+    		moStorage.put(poTPContext, oValue);
+    	}
+    }
+    
+    public clsMutableDouble getContextRatio(clsThingPresentationSingle poTPContext) {
+     	if (!poTPContext.meContentType.equals(eContext.class.getName())) {
+    		throw new java.lang.IllegalArgumentException("TP not of type "+eContext.class.getName());
+    	}    	
+    	clsMutableDouble oResult = moStorage.get(poTPContext);
+    	
+    	if (oResult == null) {
+    		oResult = new clsMutableDouble(0);
+    	}
+    	
+    	return oResult;
+    }
+    
+    public TreeMap<clsThingPresentationSingle, clsMutableDouble> getContextRatios() {
+    	return moStorage;
+    }
+    
+    public TreeMap<clsThingPresentationSingle, clsMutableDouble> getContextRatios(double prThreshold) {
+    	if (prThreshold == 0) {
+    		return getContextRatios();
+    	} else {
+    		TreeMap<clsThingPresentationSingle, clsMutableDouble> oResult = new TreeMap<clsThingPresentationSingle, clsMutableDouble>();
+        	for (Map.Entry<clsThingPresentationSingle, clsMutableDouble> oEntry:moStorage.entrySet()) {
+        		if (oEntry.getValue().doubleValue()>=prThreshold) {
+        			oResult.put(oEntry.getKey(), oEntry.getValue());
+        		} 
+        	}
+        	return oResult;
+    	}
+    }
+    
+    public void resetAllRatios() {
+    	for (Map.Entry<clsThingPresentationSingle, clsMutableDouble> oEntry:moStorage.entrySet()) {
+    		oEntry.getValue().set(0);
+    	}
     }
 }
