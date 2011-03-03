5c4f8d1563f986468a5de30d1a6779fd467ff825
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-03-03 12:56:29 +0100

    migration from svn to git and new dir step one

--- DecisionUnitInterface/src/statictools/clsGetARSPath.java
+++ DecisionUnitInterface/src/statictools/clsGetARSPath.java
@@ -4,9 +4,9 @@ public class clsGetARSPath {
 	public static String getArsPath()
 	{
 		if (System.getProperty("file.separator").equals("/"))
-			return System.getProperty("user.home") + "/SVN/ARS/PA/BWv1"; // Unix
+			return System.getProperty("user.home") + "/SVN/ARSIN_V01"; // Unix
 		else if (System.getProperty("file.separator").equals("\\"))
-			return "S:\\ARS\\PA\\BWv1"; // Windows
+			return "S:\\ARSIN_V01"; // Windows
 		else throw new NullPointerException("Spooky OS detected, can't find ARS-Root-Dir.");
 	}
 	
