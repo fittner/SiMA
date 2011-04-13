53516caafa2411b6b705e3bbb1f8affecce990f7
muchitsch <muchitsch@ICT.TUWIEN.AC.AT>
2011-04-13 17:50:37 +0200

    ADDED A EXEPTION UTIL CLASS

---
+++ BW/src/bw/exceptions/clsExeptionUtils.java
@@ -0,0 +1,33 @@
+/**
+ * clsExeptionUtils.java: BW - bw.exceptions
+ * 
+ * @author muchitsch
+ * 05.04.2011, 16:19:01
+ */
+package bw.exceptions;
+
+/**
+ * DOCUMENT (muchitsch) - insert description 
+ * 
+ * @author muchitsch
+ * 05.04.2011, 16:19:01
+ * 
+ */
+public class clsExeptionUtils {
+	
+	 public static String getCustomStackTrace(Throwable aThrowable) {
+	        //add the class name and any message passed to constructor
+	        final StringBuilder result = new StringBuilder( "ARS-EX: " );
+	        result.append(aThrowable.toString());
+	        final String NEW_LINE = System.getProperty("line.separator");
+	        result.append(NEW_LINE);
+
+	        //add each element of the stack trace
+	        for (StackTraceElement element : aThrowable.getStackTrace() ){
+	          result.append( element );
+	          result.append( NEW_LINE );
+	        }
+	        return result.toString();
+	      }
+
+}
