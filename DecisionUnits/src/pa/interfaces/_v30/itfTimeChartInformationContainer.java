30e965624f726dc35487259ce1ddddbc277e8803
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-04-18 12:44:50 +0200

    v30 time chart information container interfaces added

---
+++ DecisionUnits/src/pa/interfaces/_v30/itfTimeChartInformationContainer.java
@@ -0,0 +1,21 @@
+/**
+ * itfTimeChartInformationContainer.java: DecisionUnitMasonInspectors - inspectors.mind.pa
+ * 
+ * @author langr
+ * 04.11.2009, 19:09:18
+ */
+package pa.interfaces._v30;
+
+import java.util.ArrayList;
+
+/**
+ * DOCUMENT (langr) - insert description 
+ * 
+ * @author langr
+ * 04.11.2009, 19:09:18
+ * 
+ */
+public interface itfTimeChartInformationContainer {
+	public ArrayList<Double> getTimeChartData();
+	public ArrayList<String> getTimeChartCaptions();
+}
