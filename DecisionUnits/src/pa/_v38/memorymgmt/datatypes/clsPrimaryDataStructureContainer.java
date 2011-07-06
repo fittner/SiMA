ff8d916fd5eb6523d49edfcece54eb9543763759
wendt <wendt@ICTPC026.ICT.TUWIEN.AC.AT>
2011-07-07 00:37:08 +0200

    F46: AssignDrivemeshes and AdaptCategories moved from F35 to F46 F35: Implementation started. F18: Bugfix

--- DecisionUnits/src/pa/_v38/memorymgmt/datatypes/clsPrimaryDataStructureContainer.java
+++ DecisionUnits/src/pa/_v38/memorymgmt/datatypes/clsPrimaryDataStructureContainer.java
@@ -52,5 +52,39 @@ public class clsPrimaryDataStructureContainer extends clsDataStructureContainer 
         } catch (CloneNotSupportedException e) {
            return e;
         }
-	}		
+	}
+	
+	/**
+	 * This variant of getMoAssociatedDataStructures is only useful for containers with a "combined"
+	 * DataStructure like a TemplateImage for example. Unlike the variant without an argument this
+	 * version does not return all AssociatedDataStructures, but only the ones that are associated
+	 * with the provided argument.If the DataStructure in the container is a single Item, like
+	 * a ThingPresentationMesh, then the result will be the whole list of AssociatedDataStructures,
+	 * just like the variant without an argument.<br>
+	 * <br>
+	 * <b>NOTE</b><br>
+	 * Comparison between the argument and the RootElements of the AssociatedDataStructures is done
+	 * on the basis of the moDS_ID. While this is sufficient for the problem that made this method
+	 * necessary in the first place, this is in general <b>NOT</b> a suitable way of identifying
+	 * individual objects! Therefore this method should either be made obsolete by replacing the use
+	 * of containers with composite DataStructures by a better suited type or a new way of identifying
+	 * individual DataStructures has to be introduced.  
+	 *
+	 * @author Zottl Marcus (e0226304),
+	 * 28.06.2011, 18:47:52
+	 *
+	 * @param poAssociationRoot - an element of the DataStructure in the container for which you need
+	 * the AssociatedDataStructures.
+	 * @return an ArrayList of <i>clsAssociation</i>s that belong to the provided argument.
+	 */
+	public ArrayList<clsAssociation> getMoAssociatedDataStructures(clsDataStructurePA poAssociationRoot) {
+		ArrayList<clsAssociation> oReturnList = new ArrayList<clsAssociation>();
+		
+		for (clsAssociation oEntry : moAssociatedDataStructures) {
+			if (oEntry.getRootElement().getMoDSInstance_ID() == poAssociationRoot.getMoDSInstance_ID()) {
+				oReturnList.add(oEntry);
+			}
+		}
+		return oReturnList;
+	}
 }
