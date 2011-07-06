afe0886658b82dea385f877df931751ebe78c724
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-07-06 13:18:36 +0200

    added javadoc comments

--- DecisionUnitInterface/src/du/itf/itfDecisionUnit.java
+++ DecisionUnitInterface/src/du/itf/itfDecisionUnit.java
@@ -11,16 +11,53 @@ import du.itf.actions.itfActionProcessor;
 import du.itf.sensors.clsSensorData;
 
 /**
- * DOCUMENT (deutsch) - insert description 
+ * The interface that a decision unit has to implement to be compatible with the brain socket.
  * 
  * @author deutsch
  * 11.05.2010, 10:21:33
  * 
  */
 public interface itfDecisionUnit {
+	/**
+	 * Processes the incoming data and produces the action commands. Also known as "thinking".
+	 *
+	 * @since 06.07.2011 12:50:55
+	 *
+	 */
 	public void process();
+	
+	/**
+	 * Set the reference to the action processes. Designed using the command processor design pattern.
+	 *
+	 * @since 06.07.2011 12:50:57
+	 *
+	 * @param poActionProcessor
+	 */
 	public void setActionProcessor(itfActionProcessor poActionProcessor);
+	
+	/**
+	 * Updates the stored sensor data to the incoming values.
+	 *
+	 * @since 06.07.2011 12:51:30
+	 *
+	 * @param poSensorData
+	 */
 	public void update(clsSensorData poSensorData);
+	
+	/**
+	 * Update the action logger. Add the currently selected actions to the log.
+	 *
+	 * @since 06.07.2011 12:51:32
+	 *
+	 */
 	public void updateActionLogger();
+	
+	/**
+	 * Returns the type of the decision unit. 
+	 *
+	 * @since 06.07.2011 12:51:34
+	 *
+	 * @return
+	 */
 	public eDecisionType getDecisionUnitType();
 }
