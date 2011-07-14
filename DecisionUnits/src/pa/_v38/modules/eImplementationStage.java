619219652a7c7b5fdc90b662f036872759e581e8
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-07-14 11:38:38 +0200

    javadoc

--- DecisionUnits/src/pa/_v38/modules/eImplementationStage.java
+++ DecisionUnits/src/pa/_v38/modules/eImplementationStage.java
@@ -6,15 +6,24 @@
  */
 package pa._v38.modules;
 
+
 /**
- * DOCUMENT (deutsch) - insert description 
+ * The three different implementation stages. The three stages are: basic, draft, and final. Basic is a first 
+ * implementation that provides minimum functionality. Especially it guarantees the modules that are called after this module have at least some
+ * data to process. Draft is the implementation stage where alpha and beta stage implementations should be done. If everything is done and fits the 
+ * specs, the implementation is moved to final.
+ * 
+ * @see clsModuleBase
  * 
  * @author deutsch
- * 12.07.2010, 10:08:36
+ * 14.07.2011, 11:28:17
  * 
  */
 public enum eImplementationStage {
+	/** Basic is a first implementation that provides minimum functionality.; @since 14.07.2011 11:29:41 */
 	BASIC,
+	/** Draft is the implementation stage where alpha and beta stage implementations should be done.; @since 14.07.2011 11:30:02 */
 	DRAFT, 
+	/** If everything is done and fits the specs, the implementation is moved to final.; @since 14.07.2011 11:30:07 */
 	FINAL,
 }
--- DecisionUnits/src/pa/_v38/modules/eProcessType.java
+++ DecisionUnits/src/pa/_v38/modules/eProcessType.java
@@ -7,14 +7,17 @@
 package pa._v38.modules;
 
 /**
- * DOCUMENT (deutsch) - insert description 
+ * Defines how the data of the functional module is organized. 
  * 
  * @author deutsch
- * 11.08.2009, 12:03:37
+ * 14.07.2011, 11:34:45
  * 
  */
 public enum eProcessType {
+	/** Symbols defined according to the primary process - unorderd thing presentations.; @since 14.07.2011 11:35:16 */
 	PRIMARY,
+	/** Symbolds defined according to the secondary process - word presentations with associations to thing presentations.; @since 14.07.2011 11:35:41 */
 	SECONDARY,
+	/** Scalar values that represent electro/chemical values from within the body.; @since 14.07.2011 11:36:04 */
 	BODY
 }
