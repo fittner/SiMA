a6f0e4c6797dc51e8e9e0417a147cc8f512c8a31
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-07-14 16:03:25 +0200

    added interfaces for DT3

---
+++ DecisionUnits/src/pa/_v38/interfaces/modules/D3_1_send.java
@@ -0,0 +1,18 @@
+/**
+ * CHANGELOG
+ *
+ * 14.07.2011 deutsch - File created
+ *
+ */
+package pa._v38.interfaces.modules;
+
+/**
+ * DOCUMENT (deutsch) - insert description 
+ * 
+ * @author deutsch
+ * 14.07.2011, 15:58:23
+ * 
+ */
+public interface D3_1_send {
+	public void send_D3_1(int pnTest);
+}
