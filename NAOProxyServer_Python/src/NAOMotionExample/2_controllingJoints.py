027898ac1eec4312ac11427b35d44b25aee6c54e
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-07-14 12:44:13 +0200

    removed the projects NAO, NAOProxyClient_Java, NAOProxyServer_Python from git repository and moved them to ARS\ars_docs\code_docs\NAOvsARSIN

--- NAOProxyServer_Python/src/NAOMotionExample/2_controllingJoints.py
+++
@@ -1,8 +0,0 @@
-import config
-proxy = config.loadProxy("ALMotion")
-
-# Simple command for the HeadYaw joint at 10% max speed
-names = "HeadYaw"
-angles = 0.8
-fractionMaxSpeed = 0.1
-proxy.setAngles(names,angles,fractionMaxSpeed)
