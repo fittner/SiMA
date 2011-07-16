6d3cf268235156414e2f6da1621ce317dc82af78
wendt <wendt@ICTPC026.ICT.TUWIEN.AC.AT>
2011-07-16 10:35:22 +0200

    DataStructureComparison: New function getSpecificAssociatedContent, which is used in F45 to get DMs from the Source image and assign them to objects in the target image

--- DecisionUnits/src/pa/_v38/memorymgmt/informationrepresentation/modules/clsDataStructureComparison.java
+++ DecisionUnits/src/pa/_v38/memorymgmt/informationrepresentation/modules/clsDataStructureComparison.java
@@ -14,6 +14,7 @@ import pa._v38.storage.clsBlockedContentStorage;
 import pa._v38.tools.clsPair;
 import pa._v38.tools.clsTripple;
 import pa._v38.memorymgmt.datatypes.clsAssociation;
+import pa._v38.memorymgmt.datatypes.clsAssociationAttribute;
 import pa._v38.memorymgmt.datatypes.clsAssociationDriveMesh;
 import pa._v38.memorymgmt.datatypes.clsDataStructureContainer;
 import pa._v38.memorymgmt.datatypes.clsDataStructurePA;
@@ -446,6 +447,113 @@ public abstract class clsDataStructureComparison {
 		}
 				
 		return oMatchingDataStructureList;
+	}
+	
+	/**
+	 * DOCUMENT Compare 2 images regarding datatype and contenttype. If the perceived objects of the "FromImage" are found in the "ToImage", then the 
+	 * associated data structures (datatype and contenttype) are assigned the found structures. Only a complete type match is allowed for object matching,
+	 * i. e. the MoDS_ID is compared. The purpose of the function is to find corresponding objects in the "ToImage", where e. g. drive meshes can be attached.
+	 * This is used in F45. 
+	 * 
+	 * TODO AW: At the moment, this function is only implemented for drive meshes.
+	 *
+	 * @since 16.07.2011 10:28:21
+	 *
+	 * @param poFromImage
+	 * @param poToImage
+	 * @param poDataType
+	 * @param poContentType
+	 * @return
+	 */
+	public static ArrayList<clsPair<clsDataStructurePA, clsAssociation>> getSpecificAssociatedContent(clsPrimaryDataStructureContainer poFromImage, clsPrimaryDataStructureContainer poToImage, eDataType poDataType, String poContentType) {
+		ArrayList<clsPair<clsDataStructurePA, clsAssociation>> oRetVal = new ArrayList<clsPair<clsDataStructurePA, clsAssociation>>();
+		clsPair<clsDataStructurePA, clsAssociation> oMatch = null;
+		//Get the data structure, which could also have DMs
+		//Only DM and TP can be copied
+		
+		if ((poFromImage != null) && (poToImage != null)) {	//Catch the problem if the data structure would be null
+			//Get Target structures
+			if ((poToImage.getMoDataStructure() instanceof clsTemplateImage)==false) {
+				try {
+					throw new Exception("Error in copySpecificAssociatedContent in F45_LibidoDischarge: Only data structures consisting of clsTemplateImage canbbe used.");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			clsTemplateImage oToImageObject = (clsTemplateImage)poToImage.getMoDataStructure();
+			//TODO AW: Only Template Images, which contain TPMs are concerned, expand to other data types and nested template images
+			
+			//For each DM or TP in the associated structures in the SourceContainer
+			for (clsAssociation oAssInFromImage : poFromImage.getMoAssociatedDataStructures()) {	//The association in the source file. The root element shall be found in that target file
+				if (poDataType == eDataType.DM) {
+					if (oAssInFromImage instanceof clsAssociationDriveMesh) {
+						if (poContentType != null) {	//Add only that content type of that structure type
+							if (oAssInFromImage.getLeafElement().getMoContentType() == poContentType) {
+								oMatch = getMatchInDataStructure(oAssInFromImage, oToImageObject);
+							}
+						} else {	//Add all
+							oMatch = getMatchInDataStructure(oAssInFromImage, oToImageObject);
+						}
+					}
+				} else if (poDataType == eDataType.TP) {
+					if (oAssInFromImage instanceof clsAssociationAttribute) {
+						if (poContentType != null) {	//Add only that content type of that structure type
+							if (oAssInFromImage.getLeafElement().getMoContentType().toString() == "poContentType") {
+								oMatch = getMatchInDataStructure(oAssInFromImage, oToImageObject);
+							}
+						} else {	//Add all
+							oMatch = getMatchInDataStructure(oAssInFromImage, oToImageObject);
+						}
+					}
+				} else {
+					try {
+						throw new Exception("Error in copySpecificAssociatedContent in F45_LibidoDischarge: A not allowed datatype was used");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				
+				if (oMatch != null) {
+					oRetVal.add(oMatch);
+				}
+			}
+		}
+		
+		return oRetVal;
+	}
+	
+	/**
+	 * The root element of the association is searched in all intrinsic (de: innewohnenede, definierende) data structures in the
+	 * target image and the match is returned as a pair of the data structure of the target image and the association itself.
+	 * 
+	 * This function is a part of "getSpecificAssociatedContent"
+	 *
+	 * @since 16.07.2011 10:31:28
+	 *
+	 * @param poSourceAssociation
+	 * @param poTargetDataStructure
+	 * @return
+	 */
+	private static clsPair<clsDataStructurePA, clsAssociation> getMatchInDataStructure (clsAssociation poSourceAssociation, clsTemplateImage poTargetDataStructure) {
+		clsPair<clsDataStructurePA, clsAssociation> oRetVal = null;
+		
+		//Get root element
+		clsDataStructurePA oCompareRootElement = poSourceAssociation.getRootElement();
+		//Find the root element in the target image. Only an exact match is count
+		//1. Check if the root element is the same as the data structure in the target container
+		if ((oCompareRootElement.getMoDS_ID() == poTargetDataStructure.getMoDS_ID() && (oCompareRootElement.getMoDS_ID() > 0))) {
+			oRetVal = new clsPair<clsDataStructurePA, clsAssociation>(poTargetDataStructure, poSourceAssociation);
+		} else {
+			//2. Check if the root element can be found in the associated data structures
+			for (clsAssociation oAssToImage : poTargetDataStructure.getMoAssociatedContent()) {
+				if ((oCompareRootElement.getMoDS_ID() == oAssToImage.getLeafElement().getMoDS_ID() && (oCompareRootElement.getMoDS_ID() > 0))) {
+					oRetVal = new clsPair<clsDataStructurePA, clsAssociation>(oAssToImage.getLeafElement(), poSourceAssociation);
+					break;
+				}
+			}
+		}
+		
+		return oRetVal;
 	}
 
 	/**
