defe54ae9a23d00e69dc460fcb2b350054a0acf7
Isabella Hinterleitner <hinterleitner@ict.tuwien.ac.at>
2011-07-18 23:03:50 +0200

    implement func D3_2

--- DecisionUnits/src/pa/_v38/interfaces/modules/D3_2_send.java
+++ DecisionUnits/src/pa/_v38/interfaces/modules/D3_2_send.java
@@ -7,12 +7,12 @@
 package pa._v38.interfaces.modules;
 
 /**
- * DOCUMENT (deutsch) - insert description 
+ * DOCUMENT (deutsch) - interface for psychic energy
  * 
  * @author deutsch
  * 14.07.2011, 15:58:23
  * 
  */
 public interface D3_2_send {
-	public void send_D3_2(int pnTest);
+	public void send_D3_2(int ReducedPsychicEnergy);
 }
