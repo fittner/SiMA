5f40d1b570558c46ca381a8fd04b0c006cac6ba5
Tobias Deutsch <deutsch@ict.tuwien.ac.at>
2011-07-28 18:41:33 +0200

    removed BW. adapted javadoc

--- javadoc/gen_BW.bat
+++ javadoc/gen_BW.bat
@@ -1,8 +1,8 @@
 @REM generate BW
 "C:\Program Files\Java\jdk1.6.0_20\bin"\javadoc ^
 	-d s:\ARSIN_V01\javadoc\website\BW ^
-	-sourcepath S:\ARSIN_V01\BW\src ^
-	-overview S:\ARSIN_V01\BW\src\overview.html ^
+	-sourcepath S:\ARSIN_V01\World\src ^
+	-overview S:\ARSIN_V01\BW\World\overview.html ^
 	-subpackages ARSsim:bw:resources: ^
     -use ^
     -splitIndex ^
