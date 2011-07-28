9060e3c6eeed62f4860735a3d371e65adf08935e
Alexander Wendt <wendt@ict.tuwien.ac.at>
2011-07-28 16:42:02 +0200

    Added 2 new datatypes symmetric to the TPM: WPM and logicalComposition

---
+++ DecisionUnits/src/pa/_v38/memorymgmt/datatypes/clsLogicalStructureComposition.java
@@ -0,0 +1,91 @@
+/**
+ * clsPhysicalStructureComposition.java: DecisionUnits - pa.informationrepresentation.datatypes
+ * 
+ * @author zeilinger
+ * 23.05.2010, 21:49:15
+ */
+package pa._v38.memorymgmt.datatypes;
+
+import java.util.ArrayList;
+
+import pa._v38.tools.clsTripple;
+import pa._v38.memorymgmt.enums.eDataType;
+
+
+/**
+ * DOCUMENT (zeilinger) - insert description 
+ * 
+ * @author zeilinger
+ * 23.05.2010, 21:49:15
+ * 
+ */
+public abstract class clsLogicalStructureComposition extends clsSecondaryDataStructure {
+	protected ArrayList<clsAssociation> moAssociatedContent; 	
+	/**
+	 * @author wendt
+	 * 19.03.2011, 08:57:27
+	 * 
+	 * @return the moAssociatedContent
+	 */
+	public ArrayList<clsAssociation> getMoAssociatedContent() {
+		return moAssociatedContent;
+	}
+
+	/**
+	 * @author wendt
+	 * 19.03.2011, 08:57:27
+	 * 
+	 * @param moAssociatedContent the moAssociatedContent to set
+	 */
+	public void setMoAssociatedContent(ArrayList<clsAssociation> moAssociatedContent) {
+		this.moAssociatedContent = moAssociatedContent;
+	}
+
+	/**
+	 * DOCUMENT (wendt) - insert description 
+	 * 
+	 * @author wendt
+	 * 24.05.2010, 12:40:36
+	 * @param object2 
+	 * @param object 
+	 *
+	 */
+	public clsLogicalStructureComposition(clsTripple<Integer, eDataType, String> poDataStructureIdentifier) {
+		super(poDataStructureIdentifier);
+		moAssociatedContent = new ArrayList<clsAssociation>(); 
+	}
+	
+	/**
+	 * DOCUMENT (wendt) - insert description
+	 *
+	 * @author wendt
+	 * 17.08.2010, 21:59:35
+	 *
+	 * @param poDataStructure
+	 * @return
+	 */
+	public abstract boolean contain(clsDataStructurePA poDataStructure); 
+	
+	/**
+	 * DOCUMENT (wendt) - insert description
+	 *
+	 * @author wendt
+	 * 01.07.2010, 22:49:49
+	 *
+	 * @param poDataStructurePA
+	 */
+	public abstract void assignDataStructure(clsAssociation poDataStructurePA); 
+	
+	/**
+	 * DOCUMENT (wendt) - insert description
+	 *
+	 * @author wendt
+	 * 24.05.2010, 14:40:45
+	 *
+	 * @param poAssociatedWordPresentations
+	 */
+		
+	protected void applyAssociations(ArrayList<clsAssociation> poAssociatedDataStructures) {
+		moAssociatedContent.addAll(poAssociatedDataStructures);  
+	}
+}
---
+++ DecisionUnits/src/pa/_v38/memorymgmt/datatypes/clsWordPresentationMesh.java
@@ -0,0 +1,124 @@
+/**
+ * CHANGELOG
+ *
+ * 27.07.2011 wendt - File created
+ *
+ */
+package pa._v38.memorymgmt.datatypes;
+
+import java.util.ArrayList;
+
+import pa._v38.memorymgmt.enums.eDataType;
+import pa._v38.tools.clsTripple;
+
+/**
+ * A mesh of >=1 word presentations. If a word presentation is a word, then the word presentation is a sentence 
+ * 
+ * @author wendt
+ * 27.07.2011, 13:41:04
+ * 
+ */
+public class clsWordPresentationMesh extends clsLogicalStructureComposition {
+
+	private String moContent = "UNDEFINED";
+	
+	/**
+	 * DOCUMENT (wendt) - insert description 
+	 *
+	 * @since 27.07.2011 20:59:03
+	 *
+	 * @param poDataStructureIdentifier
+	 */
+	public clsWordPresentationMesh(
+			clsTripple<Integer, eDataType, String> poDataStructureIdentifier, 
+			ArrayList<clsAssociation> poAssociatedStructures, Object poContent) {
+		super(poDataStructureIdentifier);
+		
+		moContent = (String)poContent;
+		setAssociations(poAssociatedStructures);
+		
+		// TODO (wendt) - Auto-generated constructor stub
+	}
+
+	public String getMoContent() {
+		return moContent;
+	}
+
+	/**
+	 * @author zeilinger
+	 * 17.03.2011, 00:52:49
+	 * 
+	 * @param moContent the moContent to set
+	 */
+	public void setMoContent(String moContent) {
+		this.moContent = moContent;
+	}
+	
+	/**
+	 * DOCUMENT (zeilinger) - insert description
+	 *
+	 * @author zeilinger
+	 * 16.08.2010, 22:12:06
+	 *
+	 * @param poAssociatedTemporalStructures
+	 */
+	private void setAssociations(
+			ArrayList<clsAssociation> poAssociatedStructures) {
+		moAssociatedContent = poAssociatedStructures;
+	}
+	
+	/* (non-Javadoc)
+	 *
+	 * @since 27.07.2011 20:59:16
+	 * 
+	 * @see pa._v38.memorymgmt.datatypes.itfComparable#compareTo(pa._v38.memorymgmt.datatypes.clsDataStructurePA)
+	 */
+	@Override
+	public double compareTo(clsDataStructurePA poDataStructure) {
+		// TODO (wendt) - Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 *
+	 * @since 27.07.2011 20:59:16
+	 * 
+	 * @see pa._v38.memorymgmt.datatypes.clsLogicalStructureComposition#contain(pa._v38.memorymgmt.datatypes.clsDataStructurePA)
+	 */
+	@Override
+	public boolean contain(clsDataStructurePA poDataStructure) {
+		// TODO (wendt) - Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 *
+	 * @since 27.07.2011 20:59:16
+	 * 
+	 * @see pa._v38.memorymgmt.datatypes.clsLogicalStructureComposition#assignDataStructure(pa._v38.memorymgmt.datatypes.clsAssociation)
+	 */
+	@Override
+	public void assignDataStructure(clsAssociation poDataStructurePA) {
+		// TODO (wendt) - Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+        try {
+        	clsWordPresentationMesh oClone = (clsWordPresentationMesh)super.clone();
+           	return oClone;
+        } catch (CloneNotSupportedException e) {
+           return e;
+        }
+	}		
+	
+	@Override
+	public String toString(){
+			String oResult = "::"+this.moDataStructureType+"::";  
+			oResult += this.moDS_ID + ":" + this.moContentType + ":" + this.moContent;
+			
+			return oResult; 
+	}
+
+}
