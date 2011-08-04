8817cc3465613f1eb342fe0a76259573652b954b
Matthias Jakubec <jakubec@ict.tuwien.ac.at>
2011-08-04 16:10:20 +0200

    Merge problems solved.

--- Sim/src/PropertiesInspector/clsTestFramePropertiesInspector.java
+++ Sim/src/PropertiesInspector/clsTestFramePropertiesInspector.java
@@ -1,21 +1,37 @@
 package PropertiesInspector;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
 import config.clsProperties;
 
 
-public class clsTestFramePropertiesInspector {
+public class clsTestFramePropertiesInspector extends JFrame {
+
+	/** DOCUMENT (jakubec) - insert description; @since 03.08.2011 20:06:04 */
+	private static final long serialVersionUID = 1L;
 
 	private static String propertiesFilePath = "S:\\ARSIN_V01\\Sim\\config";
-	private static String propertiesFileName = "clemens.own.dont.touch.this.world.main.properties";
+	private static String propertiesFileName = "TestPropertiesFileForPropertyInspector3";
 
-	private static clsProperties BWProperies;
+	private static clsProperties BWProperties;
 
+	public clsTestFramePropertiesInspector() throws HeadlessException {
+		initApplication(); // Initiates the application and constructs the layout of the programs pane (window).
+	}
+	
+    private  void initApplication() {
+    	
+		BWProperties = clsProperties.readProperties(propertiesFilePath, propertiesFileName);
+		System.out.println("ConfigInspector Test!");
+		new clsPropertiesInspector(BWProperties);
+		System.out.println(BWProperties.getProperty("title"));
+    }
+
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BWProperies = clsProperties.readProperties(propertiesFilePath, propertiesFileName);
-		System.out.println("ConfigInspector Test!");
-		new clsPropertiesInspector(BWProperies);
-		System.out.println(BWProperies.getProperty("title"));
+		new clsTestFramePropertiesInspector().setVisible(true);
 	}
 }
