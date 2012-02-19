0d51d1cc4659fdb252c72143fda9cc36f8e8bd89
wendt <wendt@ICTPC026.ICT.TUWIEN.AC.AT>
2012-02-19 15:15:48 +0100

    clsSecondarySpatialTools: New tool class

---
+++ DecisionUnits/src/pa/_v38/tools/clsSecondarySpatialTools.java
@@ -0,0 +1,22 @@
+/**
+ * CHANGELOG
+ *
+ * 19.02.2012 wendt - File created
+ *
+ */
+package pa._v38.tools;
+
+/**
+ * DOCUMENT (wendt) - insert description 
+ * 
+ * @author wendt
+ * 19.02.2012, 15:14:59
+ * 
+ */
+public class clsSecondarySpatialTools {
+
+	
+	
+	
+	
+}
