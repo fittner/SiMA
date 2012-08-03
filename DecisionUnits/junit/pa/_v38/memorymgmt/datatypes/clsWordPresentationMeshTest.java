ac9277a486683ea5fef34cfa3a2b14844f8da217
Michael Havlicek <student@narph.org>
2012-08-03 20:41:17 +0200

    #unittest  * setup tests for clsWP and clsWPM

---
+++ DecisionUnits/junit/pa/_v38/memorymgmt/datatypes/clsWordPresentationMeshTest.java
@@ -0,0 +1,86 @@
+/**
+ * CHANGELOG
+ *
+ * 03.08.2012 ende - File created
+ *
+ */
+package pa._v38.memorymgmt.datatypes;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * DOCUMENT (ende) - insert description 
+ * 
+ * @author ende
+ * 03.08.2012, 20:38:45
+ * 
+ */
+public class clsWordPresentationMeshTest {
+
+	/**
+	 * DOCUMENT (ende) - insert description
+	 *
+	 * @since 03.08.2012 20:38:45
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * DOCUMENT (ende) - insert description
+	 *
+	 * @since 03.08.2012 20:38:45
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * DOCUMENT (ende) - insert description
+	 *
+	 * @since 03.08.2012 20:38:45
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * DOCUMENT (ende) - insert description
+	 *
+	 * @since 03.08.2012 20:38:45
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link pa._v38.memorymgmt.datatypes.clsWordPresentationMesh#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link pa._v38.memorymgmt.datatypes.clsWordPresentationMesh#isNullObject()}.
+	 */
+	@Test
+	public final void testIsNullObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
