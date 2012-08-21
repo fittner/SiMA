de47405191e931878199d2988f9a3abe8e38c4c5
schaat <schaat@ICT.TUWIEN.AC.AT>
2012-08-21 12:01:21 +0200

    Memory

--- DecisionUnits/src/pa/_v38/modules/F45_LibidoDischarge.java
+++ DecisionUnits/src/pa/_v38/modules/F45_LibidoDischarge.java
@@ -214,7 +214,7 @@ public class F45_LibidoDischarge extends clsModuleBaseKB implements itfInspector
 	protected void process_basic() {
 		//Get available amount of free libido 
 		mrAvailableLibido = moLibidoBuffer.send_D1_4();
-		
+		mrLibidoReducedBy =0;
 		//Clone input structure and make modification directly on the output
 		try {
 			//moPerceptionalMesh_OUT = (clsThingPresentationMesh) moPerceptionalMesh_IN.cloneGraph();
