55f61ae83c308a0616833db6489bf2a7e6784d01
herret <herret@ict.tuwien.ac.at>
2013-07-16 10:30:49 +0200

    after the nose change it is not possible that the cake is within the eatable area change arsin and cake size to solve this problem

--- World/src/bw/entities/clsCake.java
+++ World/src/bw/entities/clsCake.java
@@ -90,13 +90,13 @@ public class clsCake extends clsInanimate implements itfGetFlesh, itfAPEatable, 
 		
 		oProp.setProperty(pre+P_SHAPE+"."+clsShape2DCreator.P_DEFAULT_SHAPE, P_SHAPENAME);
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_TYPE, eShapeType.CIRCLE.name());
-		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_RADIUS, 8.0);
+		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_RADIUS, 6.0);
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_COLOR, Color.pink);
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_IMAGE_PATH, "/World/src/resources/images/schnitzl.png");
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPENAME+"."+clsShape2DCreator.P_IMAGE_POSITIONING, eImagePositioning.DEFAULT.name());	
 		
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_TYPE, eShapeType.CIRCLE.name());
-		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_RADIUS, 8.0);
+		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_RADIUS, 6.0);
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_COLOR, Color.pink);
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_IMAGE_PATH, "/World/src/resources/images/schnitzl75.png");
 		oProp.setProperty(pre+P_SHAPE+"."+P_SHAPE_75+"."+clsShape2DCreator.P_IMAGE_POSITIONING, eImagePositioning.DEFAULT.name());
