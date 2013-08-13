6b3361759bed30ed6cf84f6c0dc1d11be8239843
muchitsch <muchitsch@ICT.TUWIEN.AC.AT>
2013-08-13 16:01:15 +0200

    add 01

---
+++ DecisionUnitInterface/src/du/enums/eSaliency.java
@@ -0,0 +1,23 @@
+/**
+ * CHANGELOG
+ *
+ * 13.08.2013 muchitsch - File created
+ *
+ */
+package du.enums;
+
+/**
+ * DOCUMENT (muchitsch) - insert description 
+ * 
+ * @author muchitsch
+ * 13.08.2013, 15:56:04
+ * 
+ */
+public enum eSaliency {
+
+	UNDEFINED,
+	LOW,
+	MEDIUM,
+	HIGH,
+	VERYHIGH
+}
