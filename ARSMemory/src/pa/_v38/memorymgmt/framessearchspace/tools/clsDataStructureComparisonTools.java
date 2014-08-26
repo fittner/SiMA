7eef9f7fce6c1f633f35ee8a9107af1c61290797
Kollmann <kollmann@ict.tuwien.ac.at>
2014-08-26 12:51:09 +0200

    Added a simple check to getCompleteMesh(...) to handle WPMs that do not have a memory representation (this happens for example if the TPM:IMAGE has no WPM:IMAGE associated)

--- ARSMemory/src/pa/_v38/memorymgmt/framessearchspace/tools/clsDataStructureComparisonTools.java
+++ ARSMemory/src/pa/_v38/memorymgmt/framessearchspace/tools/clsDataStructureComparisonTools.java
@@ -840,8 +840,13 @@ public abstract class clsDataStructureComparisonTools {
 			}
 		}
 		
-		//Complement all associations in the other structures
-		clsMeshTools.setInverseAssociations(oRetVal);
+		if(oRetVal != null) {
+			//Complement all associations in the other structures
+			clsMeshTools.setInverseAssociations(oRetVal);
+		} else {
+			log.error("Could not complete WPM {} - possibly because it is not found in memory (did you forget to connect the TPM and WPM in memory?)", poInput.getContent());
+			log.error("Stack: ", new Exception());
+		}
 		
 		return oRetVal;
 	}
