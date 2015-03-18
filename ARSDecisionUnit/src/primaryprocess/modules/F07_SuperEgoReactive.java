0a515a72dc403c16d19e00f93fe7be53efa0badb
Kollmann <kollmann@ict.tuwien.ac.at>
2015-03-18 12:10:05 +0100

    Changed GIVE calibration

--- ARSDecisionUnit/src/primaryprocess/modules/F07_SuperEgoReactive.java
+++ ARSDecisionUnit/src/primaryprocess/modules/F07_SuperEgoReactive.java
@@ -526,7 +526,7 @@ public class F07_SuperEgoReactive extends clsModuleBase
                             //Kollmann: according to KD, the intensity of the confclicting emotion should have less impact than the super ego rule strength
                             //          (0.5 is a completely arbitrary value)
                             //          also: according to KD, the difference between allowed intensity and actual intensity should be used.
-                            rConflictTension = (moRules.get(i).getSuperEgoRuleStrength() * Math.min(prReceivedPsychicEnergy / mrInitialRequestIntensity, 1)) + (rIntensityOfConflictingEmotions * /*arbitrary->*/0.5);
+                            rConflictTension = (moRules.get(i).getSuperEgoRuleStrength() * Math.min(Math.pow(prReceivedPsychicEnergy, 0.3), 1)) + (rIntensityOfConflictingEmotions * /*arbitrary->*/0.5);
                             
                             if (!moForbiddenEmotions.contains(moRules.get(i).getForbiddenEmotion(fe))) 
                                 moForbiddenEmotions.add(new clsSuperEgoConflictEmotion(moRules.get(i).getForbiddenEmotion(fe), rConflictTension));
--- ARSDecisionUnit/src/primaryprocess/modules/F71_CompositionOfExtendedEmotion.java
+++ ARSDecisionUnit/src/primaryprocess/modules/F71_CompositionOfExtendedEmotion.java
@@ -335,7 +335,7 @@ public class F71_CompositionOfExtendedEmotion extends clsModuleBase implements I
                             //Kollmann: according to KD, the intensity of the confclicting emotion should have less impact than the super ego rule strength
                             //          (0.5 is a completely arbitrary value)
                             //          also: according to KD, the difference between allowed intensity and actual intensity should be used.
-                            rConflictTension = (moRules.get(i).getSuperEgoRuleStrength() * Math.min(prReceivedPsychicEnergy / mrInitialRequestIntensity, 1)) + (rIntensityOfConflictingEmotions * /*arbitrary->*/0.5);
+                            rConflictTension = (moRules.get(i).getSuperEgoRuleStrength() * Math.min(Math.pow(prReceivedPsychicEnergy, 0.3), 1)) + (rIntensityOfConflictingEmotions * /*arbitrary->*/0.5);
                             
                             if (!moForbiddenEmotions.contains(moRules.get(i).getForbiddenEmotion(fe))) 
                                 moForbiddenEmotions.add(new clsSuperEgoConflictEmotion(moRules.get(i).getForbiddenEmotion(fe), rConflictTension));
