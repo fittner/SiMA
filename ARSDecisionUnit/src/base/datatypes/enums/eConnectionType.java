d5b97e5dc34abe0247220465fd644f3813ad44a2
Kollmann <kollmann@ict.tuwien.ac.at>
2015-06-15 15:12:21 +0200

    Added enum for connection type

---
+++ ARSDecisionUnit/src/base/datatypes/enums/eConnectionType.java
@@ -0,0 +1,19 @@
+/**
+ * CHANGELOG
+ *
+ * 03.06.2015 Kollmann - File created
+ *
+ */
+package base.datatypes.enums;
+
+/**
+ * DOCUMENT (Kollmann) - insert description 
+ * 
+ * @author Kollmann
+ * 03.06.2015, 15:35:02
+ * 
+ */
+public enum eConnectionType {
+    INTERNAL,
+    EXTERNAL
+}
