b65bace2261177e4e565b4431836b269fd0dfa0a
Christian Brandstaetter <brandstaetter@imp.ac.at>
2018-10-18 12:08:43 +0200

    initial integration of influxdb

---
+++ ARSDecisionUnit/src/base/tools/clsPost.java
@@ -0,0 +1,56 @@
+package base.tools;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class clsPost {
+    public static void sendInflux(String module, String measurement, String value) {
+
+        String url = "http://localhost:8086/write?db=sima";
+        URL obj;
+        try {
+            obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // add reuqest header
+            con.setRequestMethod("POST");
+            // con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            // con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+            String urlParameters = measurement + ",module=" + module + " value=" + value;
+
+            // Send post request
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            //System.out.println("\nSending 'POST' request to URL : " + url);
+            //System.out.println("Post parameters : " + urlParameters);
+            //System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            // TODO (brandy) - Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
